e3dafbd61761c2696686dd0de8b4c5b75d618a22
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-05-17 18:44:47 +0100

    restructure vec3

--- math/vec3.zig
+++ math/vec3.zig
@@ -1,33 +1,25 @@
-pub fn Vec3(comptime T: type) type {
-    return struct {
-        const self = @This();
-        x: T,
-        y: T,
-        z: T,
+pub const Vec3 = struct {
+    x: i32,
+    y: i32,
+    z: i32,
 
-        pub fn init(x: T, y: T, z: T) self {
-            return .{
-                .x = x,
-                .y = y,
-                .z = z,
-            };
-        }
-    };
-}
+    pub fn init(x: i32, y: i32, z: i32) Vec3 {
+        return .{
+            .x = x,
+            .y = y,
+            .z = z,
+        };
+    }
 
-//const Vec3 = union {
-//    const _ = struct {
-//        x: i32,
-//        y: i32,
-//        z: i32,
-//
-//        fn init(x: i32, y: i32, z: i32) Vec3 {
-//            return Vec3{
-//                .x = x,
-//                .y = y,
-//                .z = z,
-//            };
-//        }
-//    };
-//    arr: [3]i32,
-//};
+    pub fn add(self: *Vec3, other: Vec3) void {
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
+    }
+
+    pub fn Add(self: Vec3, other: Vec3) Vec3 {
+        var res_vec = self;
+        res_vec.add(other);
+        return res_vec;
+    }
+};
