ab327dd378d7637d4b3d058861d526f56d0adf00
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-05-19 22:15:19 +0100

    use T instead of spamming i32, can test floats or anything else easily now too

--- math/test_vec3.zig
+++ math/test_vec3.zig
@@ -2,13 +2,14 @@ const Vec3 = @import("vec3.zig").Vec3;
 const expect = @import("std").testing.expect;
 const print = @import("std").debug.print;
 
-const x: i32 = 1;
-const y: i32 = 2;
-const z: i32 = 3;
+const T = f32;
+const x: T = 1;
+const y: T = 2;
+const z: T = 3;
 
 test "Vec3.init()" {
     print("TEST: Vec3.init()", .{});
-    const v = Vec3(i32).init(x, y, z);
+    const v = Vec3(T).init(x, y, z);
     try expect(v.x == x);
     try expect(v.y == y);
     try expect(v.z == z);
@@ -17,8 +18,8 @@ test "Vec3.init()" {
 
 test "Vec3.add()" {
     print("TEST: Vec3.add()", .{});
-    var v1 = Vec3(i32).init(x, y, z);
-    const v2 = Vec3(i32).init(z, y, x);
+    var v1 = Vec3(T).init(x, y, z);
+    const v2 = Vec3(T).init(z, y, x);
 
     v1.add(v2);
 
@@ -30,10 +31,10 @@ test "Vec3.add()" {
 
 test "Vec3.Add()" {
     print("TEST: Vec3.Add()", .{});
-    const v1 = Vec3(i32).init(x, y, z);
-    const v2 = Vec3(i32).init(z, y, x);
+    const v1 = Vec3(T).init(x, y, z);
+    const v2 = Vec3(T).init(z, y, x);
 
-    const v_res = Vec3(i32).Add(v1, v2);
+    const v_res = Vec3(T).Add(v1, v2);
 
     try expect(v_res.x == v1.x + v2.x);
     try expect(v_res.y == v1.y + v2.y);
@@ -43,8 +44,8 @@ test "Vec3.Add()" {
 
 test "Vec3.sub()" {
     print("TEST: Vec3.sub()", .{});
-    var v1 = Vec3(i32).init(x, y, z);
-    const v2 = Vec3(i32).init(z, y, x);
+    var v1 = Vec3(T).init(x, y, z);
+    const v2 = Vec3(T).init(z, y, x);
 
     v1.sub(v2);
 
@@ -56,10 +57,10 @@ test "Vec3.sub()" {
 
 test "Vec3.Sub()" {
     print("TEST: Vec3.Sub()", .{});
-    const v1 = Vec3(i32).init(x, y, z);
-    const v2 = Vec3(i32).init(z, y, x);
+    const v1 = Vec3(T).init(x, y, z);
+    const v2 = Vec3(T).init(z, y, x);
 
-    const v_res = Vec3(i32).Sub(v1, v2);
+    const v_res = Vec3(T).Sub(v1, v2);
 
     try expect(v_res.x == v1.x - v2.x);
     try expect(v_res.y == v1.y - v2.y);
@@ -69,8 +70,8 @@ test "Vec3.Sub()" {
 
 test "Vec3.scale()" {
     print("TEST: Vec3.scale()", .{});
-    var v = Vec3(i32).init(x, y, z);
-    const factor: i32 = z;
+    var v = Vec3(T).init(x, y, z);
+    const factor: T = z;
 
     v.scale(factor);
 
@@ -83,10 +84,10 @@ test "Vec3.scale()" {
 
 test "Vec3.Scale()" {
     print("TEST: Vec3.Scale()", .{});
-    const v = Vec3(i32).init(x, y, z);
-    const factor: i32 = z;
+    const v = Vec3(T).init(x, y, z);
+    const factor: T = z;
 
-    const res_vec = Vec3(i32).Scale(v, factor);
+    const res_vec = Vec3(T).Scale(v, factor);
 
     try expect(res_vec.x == x * factor);
     try expect(res_vec.y == y * factor);
@@ -97,10 +98,10 @@ test "Vec3.Scale()" {
 
 test "Vec3.Dot()" {
     print("TEST: Vec3.Dot()", .{});
-    const v1 = Vec3(i32).init(x, y, z);
-    const v2 = Vec3(i32).init(z, y, x);
+    const v1 = Vec3(T).init(x, y, z);
+    const v2 = Vec3(T).init(z, y, x);
 
-    const dot_res = Vec3(i32).Dot(v1, v2);
+    const dot_res = Vec3(T).Dot(v1, v2);
     const test_res = (x * z) + (y * y) + (z * x);
 
     try expect(dot_res == test_res);
