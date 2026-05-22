cefe2787b15d786fa364ed053941efc3f350a8b3
Eoghan Murphy <stiger.home@gmail.com>
2026-05-22 22:52:16 +0100

    Fix silly @This() calls

--- lib/math/vec4.zig
+++ lib/math/vec4.zig
@@ -14,19 +14,19 @@ pub fn Vec4(comptime T: type) type {
         z: T,
         w: T,
 
-        pub fn init(x: T, y: T, z: T, w: T) @This() {
+        pub fn init(x: T, y: T, z: T, w: T) Vec4(T) {
             // TODO: w == 0
             // TODO: overflow division
-            var res: @This() = undefined;
+            var res: Vec4(T) = undefined;
             if (@typeInfo(T) == .int) {
-                res = @This(){
+                res = .{
                     .x = @divFloor(x, w),
                     .y = @divFloor(y, w),
                     .z = @divFloor(z, w),
                     .w = 1,
                 };
             } else {
-                res = @This(){
+                res = .{
                     .x = x / w,
                     .y = y / w,
                     .z = z / w,
@@ -37,8 +37,8 @@ pub fn Vec4(comptime T: type) type {
             return res;
         }
 
-        pub fn init_Vec3(v: @This()) @This() {
-            return @This().init{
+        pub fn init_Vec3(v: Vec3(T)) Vec4(T) {
+            return Vec4(T).init{
                 .x = v.x,
                 .y = v.y,
                 .z = v.z,
